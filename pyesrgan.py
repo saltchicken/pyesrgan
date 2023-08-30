17c7be0115f14de8b7df96ef67bd8d107853b04d
saltchicken <john.eicher89@gmail.com>
2023-08-29 18:28:49 -0700

    Refactor pyesrgan to be imported in other scripts

--- pyesrgan.py
+++ pyesrgan.py
@@ -7,15 +7,20 @@ def main():
     parser.add_argument('-i', '--input', required=True, help='Input image')
     parser.add_argument('-o', '--output', default='__', type=str, help='Output file')
     args = parser.parse_args()
+    run_esrgan(args.input, args.output)
     
+    
+    
+def run_esrgan(input, output='__'):
     current_directory = os.path.dirname(os.path.abspath(__file__))
     executable_path = os.path.join(current_directory, 'esrgan/realesrgan-ncnn-vulkan.exe')
-    if args.output is not '__':
-        parameters = ['-i', args.input, '-o', args.output, '-n', 'x4']
+    if output is not '__':
+        parameters = ['-i', input, '-o', output, '-n', 'x4']
     else:
-        parameters = ['-i', args.input, '-o', 'resized_' + args.input, '-n', 'x4']
+        parameters = ['-i', input, '-o', 'resized_' + input, '-n', 'x4']
         
     subprocess.run([executable_path] + parameters)
+    
     
 if __name__ == "__main__":
     main()

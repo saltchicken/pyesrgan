c4b852aa8bf1d5f5317f3a1188c3816d0d7ce6f5
saltchicken <john.eicher89@gmail.com>
2023-08-29 18:35:36 -0700

    Fixed output filename issue

--- pyesrgan.py
+++ pyesrgan.py
@@ -5,19 +5,16 @@ def main():
     parser = argparse.ArgumentParser(description="Simple ESRGAN implementation")
     
     parser.add_argument('-i', '--input', required=True, help='Input image')
-    parser.add_argument('-o', '--output', default='__', type=str, help='Output file')
+    parser.add_argument('-o', '--output', default='resized.png', type=str, help='Output file')
     args = parser.parse_args()
     run_esrgan(args.input, args.output)
     
     
     
-def run_esrgan(input, output='__'):
+def run_esrgan(input, output):
     current_directory = os.path.dirname(os.path.abspath(__file__))
     executable_path = os.path.join(current_directory, 'esrgan/realesrgan-ncnn-vulkan.exe')
-    if output is not '__':
-        parameters = ['-i', input, '-o', output, '-n', 'x4']
-    else:
-        parameters = ['-i', input, '-o', 'resized_' + input, '-n', 'x4']
+    parameters = ['-i', input, '-o', output, '-n', 'x4']
         
     subprocess.run([executable_path] + parameters)
     
